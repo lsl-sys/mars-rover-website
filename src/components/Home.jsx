c294c8036e3bb750e4320a0d4f7c436aadead1a0
lsl-sys <m15397763602@163.com>
2025-09-04 17:59:13 +0800

    修复

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -97,8 +97,9 @@ const Home = () => {
                   className="promo-video" 
                   controls 
                   preload="metadata"
+                  poster="/mars_rover.jpg"
                 >
-                  <source src="/视频/视频1.mp4" type="video/mp4" />
+                  <source src="/Video/VIDEO.mp4" type="video/mp4" />
                   您的浏览器不支持视频播放。
                 </video>
               </div>
